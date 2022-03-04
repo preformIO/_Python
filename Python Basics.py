563c66f533afce34a0c13ab79fc591ddb77de01b
david allen <allend5@rpi.edu>
2022-03-04 12:04:44 -0500

    Variables and datatype defs, web python editor URL

--- Python Basics.py
+++ Python Basics.py
@@ -1,14 +1,49 @@
 # %% [markdown]
 # # Python Basics
+#  * Variables
 #  * Data types
 #  * Operators
-#  
+# 
+
+# %% [markdown]
+# ## Variables
+#  * are placeholders for values in computer's memory
+#  * calling a variable by name returns its value in memory
+
+# %%
+# set variable to a string value of "Hello World!"
+message = "Hello World!"
+print(message)
+
+# set variable a to a value of 1 (integer)
+an_integer = 1
+
+# print the value of the vaiable to the console by callin the variable by name
+print(an_integer)
+
+# set another variable to int value of 2
+another_integer = 2
+
+# set yet another integer to the value of the sum of our first two integer
+integer_sum = an_integer + another_integer
+
+# print the sum of the first two integers
+print(integer_sum)
+
+# set variable to a float value of 1.3
+a_float = 1.3
+
+print(a_float)
 
 # %% [markdown]
 # ## Data types
+#  * strings = ""
+#  * integers = 1
+#  * floats = 1.0
+
 
 # %%
-# Interger
+# Integer
 i = 1
 i2 = int(2.5) # ingores all decimal point information
 print("i =", i)
@@ -23,7 +58,7 @@ print("f + i =", f + i)
 print("f * 2 =", f * 2, '\n')
 
 # %%
-# Strings
+# String
 # -------------------
 # basic string with single quotes
 ss = 'Hello World! Is the "default" message for a beginning application.'
@@ -55,23 +90,25 @@ ratio = 35/3
 print(f"Ratio '{ratio}' converts to '{ratio *100:,.2f}%'")
 
 # %%
-# Lists - 1D array
+# Lists
+# ----------------
+# 1D array -- a 1x15 array explicitly defined... 
 l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] 
-# a 1x15 array explicitly defined... also example of zero indexing item addresses
 
 print(f"List 'l' = \n{l}\n")
 
 # %%
+# example of building a list by appending items to an empty list
 l = []
 for i in range(15):
   l.append(i)
 print(f"List 'l' built with traditional for i in range(15) = \n{l}")
 
-# example using list comprehension
+# example of building a list using list comprehension
 l = [i for i in range(15)] 
 print(f"List 'l' built with range(15) = \n{l}")
 
-# example of using list comprehension and random
+# using list comprehension and random
 import random
 lrandom = [
   random.randint(1,5) for i in range(15)
@@ -79,7 +116,8 @@ lrandom = [
 print(f"List 'lrandom' = \n{lrandom}\n")
 
 # %%
-l = [0] * 15 # using list * operator
+# using list * operator
+l = [0] * 15 
 print(f"List 'l' = \n{l}")
 print(f"List element 'l[14]' = {l[14]}\n")
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # _Python
 
 A collection of python resources I have created to help students learn basic programming in Python and a bit about data analysis and visualization tools.
+
+## Resources
+[Online Python IDE](https://www.online-python.com/)
